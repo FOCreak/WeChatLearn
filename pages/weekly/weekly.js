1a76f9a64df3d26fcc08cb81310806ff8261c2ed
youname <you@mail.com>
2020-06-03 23:04:40 +0800

    Initial Commit

---
+++ pages/weekly/weekly.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    weeklyMovieList:[
+      {
+        name:'荒野1',
+        comment:'xxxxxxsssssssssssssssx',
+        imagePath:'/images/02789_owenslake_2560x1600.jpg',
+        isHighlyRecommended: true
+      },
+      {
+        name:'荒野2',
+        comment:'xxxxsssssssssssssssx',
+        imagePath:'/images/02789_owenslake_2560x1600.jpg',
+        isHighlyRecommended: false
+      },
+      {
+        name:'荒野3',
+        comment:'xxxxxxsssssssssx',
+        imagePath:'/images/02789_owenslake_2560x1600.jpg',
+        isHighlyRecommended: true
+      }
+    ] ,
+    count: 163,
+    score:15
+  }
+})
